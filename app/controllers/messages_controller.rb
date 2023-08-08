b9244179e8286d54061687d1a2fdbf1d16f6374b
kazzz8 <wmat441@gmail.com>
2023-08-08 20:19:16 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id]) #ページのパスが/rooms/:room_id/messagesとなっている。パスにroom_idが含まれているため、paramsというハッシュオブジェクトの中に、room_idという値が存在している。それをparams[:room_id]と書いて取得している。
+  end
+
+  def create
+    @room = Room.find(params[:room_id]) #今いるroomのインスタンスを生成する。次の@room.messages.newで必要。
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room) #room_messages_pathはroomのidによって変わる。なので今インスタンスを持っているroomのidを記述しなければならない。
+    else
+      render :index, status: :unprocessable_entity #アクション名に対応するビューを表示。今回だとindex。renderは今持っているデータをそのままもう一度ビューに送るので表示内容が変わらない。
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id) #messageにはuser_idが必要なのでマージして付与している。permitは許可するカラムを指定している。
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :users, onle: [:edit, :update]
-  resources :rooms, onle: [:new, :create]
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create] #ネストさせることによって、メッセージに紐付いているチャットルームのidという意味になる
+  end
 end
