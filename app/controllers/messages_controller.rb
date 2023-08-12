9f3952a7c270c86f18be14911cd4fe927f0768bd
kazzz8 <wmat441@gmail.com>
2023-08-12 12:12:16 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id]) #ページのパスが/rooms/:room_id/messagesとなっている。パスにroom_idが含まれているため、paramsというハッシュオブジェクトの中に、room_idという値が存在している。それをparams[:room_id]と書いて取得している。
+    @messages = @room.messages.includes(:user) #上で@roomに今いるページのルームを格納しているのでそれを使用。roomテーブルとmessagesテーブルは紐付いているので、多分この書き方で対象のroomのmessagesを取得できるんだと思う。さらに(:user)とすることでユーザー情報をまとめて取得することができる（これはよくわからん）
   end
 
   def create
@@ -10,6 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room) #room_messages_pathはroomのidによって変わる。なので今インスタンスを持っているroomのidを記述しなければならない。
     else
+      @messages = @room.messages.includes(:user) #messageの投稿に失敗したときにメッセージ一覧に戻れるように定義
       render :index, status: :unprocessable_entity #アクション名に対応するビューを表示。今回だとindex。renderは今持っているデータをそのままもう一度ビューに送るので表示内容が変わらない。
     end
   end
