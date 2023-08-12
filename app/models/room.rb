52fe77a96a0bd4adbb3d6d2641c023dc69674fb3
kazzz8 <wmat441@gmail.com>
2023-08-12 14:54:33 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
-  has_many :room_users
+  has_many :room_users, dependent: :destroy #userを消してはいけない。room_usersテーブルのレコードを消す
   has_many :users, through: :room_users
-  has_many :messages
+  has_many :messages, dependent: :destroy #dependentは「依存している」という意味。つまりmessagesはroomに依存しているということになる。そしてdestroyを指定することで、roomが削除されたときにmessagesも一緒に削除されるということになる。
 
   validates :name, presence: true
   
